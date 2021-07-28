1c88a0235dc46186ad57758a2847eaad7c5f4f15
TheGloriousGenesis <am16g14@soton.ac.uk>
2021-07-28 21:18:07 +0100

    changed player controls back to joystick

---
+++ Assets/Scripts/Generators/CoinGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Behaviour.Entities;
+using UnityEngine;
+using Utilities;
+
+namespace GeneticAlgorithms.Generators
+{
+    public class CoinGenerator : MonoBehaviour
+    {
+        public PrefabFactory prefabs;
+
+        public void PlaceCoins()
+        {
+            PlaceCoins(Utility.GamePlacement);
+        }
+
+        private void PlaceCoins(Dictionary<Vector3, BlockType> gamePlacement)
+        {
+            var tmp = gamePlacement.ToList();
+
+            foreach (var i in tmp)
+            {
+                if (i.Value != BlockType.NONE) continue;
+                var coin = Instantiate(prefabs[BlockType.COIN], i.Key + Vector3.up * 2, Quaternion.identity);
+                coin.transform.parent = transform;
+                Utility.GamePlacement[i.Key] = BlockType.COIN;
+            }
+        }
+    }
+}
