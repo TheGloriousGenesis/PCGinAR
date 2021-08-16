a406e218d9827680653beb03d351d69c56c08c73
TheGloriousGenesis <am16g14@soton.ac.uk>
2021-08-17 00:24:33 +0100

    Implemented scaling function

---
+++ Assets/Scripts/Utilities/DesignPatterns/SerializedQuaternion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+namespace Utilities.DesignPatterns
+{
+    [Serializable]
+    public class SerializedQuaternion
+    {
+        public float x;
+        public float y;
+        public float z;
+        public float w;
+
+        public SerializedQuaternion(float rX, float rY, float rZ, float rW)
+        {
+            x = rX;
+            y = rY;
+            z = rZ;
+            w = rW;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("[{0}, {1}, {2}, {3}]", x, y, z, w);
+        }
+        
+        public static implicit operator Quaternion(SerializedQuaternion rValue)
+        {
+            return new Quaternion(rValue.x, rValue.y, rValue.z, rValue.w);
+        }
+        
+        public static implicit operator SerializedQuaternion(Quaternion rValue)
+        {
+            return new SerializedQuaternion(rValue.x, rValue.y, rValue.z, rValue.w);
+        }
+    }
+}
---
+++ Assets/Scripts/Utilities/DesignPatterns/SerializedVector3.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+namespace Utilities.DesignPatterns
+{
+    [Serializable]
+    public struct SerializedVector3
+    {
+        public float x;
+        public float y;
+        public float z;
+
+        public SerializedVector3(float paramX, float paramY, float paramZ)
+        {
+            x = paramX;
+            y = paramY;
+            z = paramZ;
+        }
+        
+        public override string ToString()
+        {
+            return String.Format("[{0}, {1}, {2}]", x, y, z);
+        }
+        
+        public static implicit operator Vector3(SerializedVector3 rValue)
+        {
+            return new Vector3(rValue.x, rValue.y, rValue.z);
+        }
+        
+        public static implicit operator SerializedVector3(Vector3 rValue)
+        {
+            return new SerializedVector3(rValue.x, rValue.y, rValue.z);
+        }
+    }
+}
